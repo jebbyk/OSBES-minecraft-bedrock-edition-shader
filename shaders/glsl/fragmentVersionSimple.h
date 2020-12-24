953c857ae03af2b5dd52e6121e8cff70d242e009
Daniel Bradbury <danieljamesbradbury@hotmail.com>
2020-12-24 17:15:39 +0000

    SPAG corrections H-N

--- shaders/glsl/fragmentVersionSimple.h
+++ shaders/glsl/fragmentVersionSimple.h
@@ -1,11 +1,11 @@
 // To use centroid sampling we need to have version 300 ES shaders, which requires changing:
-// attribute to in
-// varying to out when in vertex shaders or in when in fragment shaders
-// defining an out vec4 FragColor and replacing uses of gl_FragColor with FragColor
-// texture2D to texture
+// - Attribute to in
+// - Varying to out when in vertex shaders or in when in fragment shaders
+// - Defining an out vec4 FragColor and replacing uses of gl_FragColor with FragColor
+// - texture2D to texture
 #if __VERSION__ >= 300
 
-// version 300 code
+// Version 300 code
 
 #define varying in
 #define texture2D texture
@@ -14,6 +14,6 @@ out vec4 FragColor;
 
 #else
 
-// version 100 code
+// Version 100 code
 
 #endif
