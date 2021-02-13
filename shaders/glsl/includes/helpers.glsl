9f22d3e9304e8b291ca37bd16236d6d46449c9b5
jebbyk <zaicev9797@gmail.com>
2021-02-13 17:32:12 +0600

    move fresnel calculation to separate file

--- shaders/glsl/includes/helpers.glsl
+++ shaders/glsl/includes/helpers.glsl
@@ -5,3 +5,15 @@ highp vec3 calculateRawNormalVector(highp vec3 position){
 
 	return normalize(cross(dp1, dp2));
 }
+
+
+float calculateFresnel(vec3 initialNormalVector, vec3 normalVector, vec3 viewDir, float isWater, float isRain, float wetness){
+    float flatFresnel = min(1.0 - dot(initialNormalVector, viewDir), 1.0);
+	float detailedFresnel = min(1.0 - dot(normalVector, viewDir), 1.0);
+
+	detailedFresnel = pow(detailedFresnel, 6.0);
+	flatFresnel = pow(flatFresnel, 3.0);
+
+	detailedFresnel = mix(detailedFresnel, flatFresnel, isWater);
+	return mix(detailedFresnel, flatFresnel, wetness * isRain * initialNormalVector.g * (1.0 - isWater));
+}
