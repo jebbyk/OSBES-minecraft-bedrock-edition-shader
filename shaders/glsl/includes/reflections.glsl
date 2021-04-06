c472e3c9aa0e22728bc6890081b721ae6a4fa03f
sad <irsadkun4@gmail.com>
2021-04-06 14:44:10 +0700

    fix cloud reflection and update manifest.json

--- shaders/glsl/includes/reflections.glsl
+++ shaders/glsl/includes/reflections.glsl
@@ -2,37 +2,41 @@
 #include "../uniformShaderConstants"
 #include "random.glsl"
 
-vec4 buildRawSkyReflection(vec3 relativePosition, vec3 resultLighting)
+vec4 buildRawSkyReflection(vec3 reflectedVector, vec3 resultLighting)
 {
-	float horizonLine =  1.0 - (abs(relativePosition.y) / length(relativePosition.xyz));
-	vec3 horizonColor = resultLighting;
+	vec3 upPosition = normalize(vec3(0.0, abs(relativePosition.y), 0.0));
+
+	highp float isZenithSky = dot(reflectedVector, upPosition);
+	highp float horizonLine = max(0.0, 1.0-exp(-0.25 / abs(isZenithSky)));
+		isZenithSky = max(0.0, isZenithSky);
+
+	vec3 horizonColor = pow(resultLighting * vec3(0.5, 0.7, 1.0), vec3(0.5));
 	vec3 zenithColor = vec3(0.25, 0.5, 1.0) * length(resultLighting);
-	return vec4(mix(zenithColor, horizonColor, horizonLine), horizonLine);
+	return vec4(mix(zenithColor, horizonColor, horizonLine), isZenithSky);
 }
 
-vec3 buildSkyPlaneReflection(vec3 relativePosition, vec3 normalColor, vec4 skyLightReflected, float isRain, float roughness){
+vec3 buildSkyPlaneReflection(vec3 reflectedVector, vec4 skyLightReflected, float isRain, float roughness){
 
-	highp vec2 cldCoord = -relativePosition.xz;
-	cldCoord += cldCoord * normalColor.rb * 0.5;
-	cldCoord /= length(relativePosition.y);
-	
-	float clouds = cloudsPerlin(0, cldCoord / 16.0);
+	highp vec3 cldCoord = reflectedVector;
+		cldCoord /= cldCoord.y;
+
+	float clouds = cloudsPerlin(2, cldCoord.xz / 16.0);
 
 	clouds = pow(clamp(clouds * 1.75, 0.0, 1.0), mix(roughness * 32.0, 2.0, isRain));
-	
+
 	vec3 clearSkyCloudsColor = vec3(1.75) * pow(length(skyLightReflected.gb), 2.0);
 	vec3 rainSkyCloudsColor = skyLightReflected.rgb * 0.75;
-	
+
 	vec3 cloudsColor = mix(clearSkyCloudsColor, rainSkyCloudsColor, isRain);
 
-	return mix(skyLightReflected.rgb, cloudsColor, clouds * (1.0 - clamp(pow(skyLightReflected.a * 1.0625, 4.0), 0.0, 1.0)));
+	return mix(skyLightReflected.rgb, cloudsColor, clouds * skyLightReflected.a);
 }
 
 
 vec4 calculateMainLightsReflection(vec3 normalVector, vec3 viewDir, vec4 mainLightDiffused, float shininess, float isRain, float isSunrize){
 	// Blinn-phong
 	vec3 fakeLightDir = vec3(0.975, 0.025, 0.0);
-	vec3 halfwayDir = normalize(fakeLightDir + viewDir); 
+	vec3 halfwayDir = normalize(fakeLightDir + viewDir);
 	float spec = pow(max(dot(normalVector, halfwayDir), 0.0), shininess) * (1.0 - isRain) * 10.0;
 
 	// Phong (fake halo effect)
