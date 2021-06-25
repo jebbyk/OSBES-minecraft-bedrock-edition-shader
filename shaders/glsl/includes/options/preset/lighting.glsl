5c5bb7637b6b414931f605cfc3a6402edb7cfff2
OpenSauce04 <danieljamesbradbury@hotmail.com>
2021-06-25 13:37:17 +0100

    Corrected "dirrection" to "direction"

--- shaders/glsl/includes/options/preset/lighting.glsl
+++ shaders/glsl/includes/options/preset/lighting.glsl
@@ -1,6 +1,6 @@
 #define POINT_LIGHTS_TINT vec3(1.0, 0.66, 0.33)
 #define POINT_LIGHTS_INTENSITY 2.0
-#define MAIN_LIGHT_DIRRECTION vec3(0.9, 0.5, 0.0) //used only for reflection
+#define MAIN_LIGHT_DIRECTION vec3(0.9, 0.5, 0.0) //used only for reflection
 #define MAIN_LIGHT_SUN_COLOR vec3(1.0, 0.88, 0.69)
 #define MAIN_LIGHT_MOON_COLOR vec3(0.35, 0.6,1.0)
 #define SKY_LIGHT_SATURATION 0.6 //horizon color and diffuse sky light
