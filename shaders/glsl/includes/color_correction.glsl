104d748df20ba3056b44d851595a600d40fc0a21
jebbyk <zaicev9797@gmail.com>
2021-07-04 20:31:58 +0600

    add tonemapping preset settings

--- shaders/glsl/includes/color_correction.glsl
+++ shaders/glsl/includes/color_correction.glsl
@@ -1,18 +1,15 @@
 vec3 colorCorrection(vec3 diffuse){
-    vec3 tint = vec3(1.5, 1.25, 1.0)*2.5;
-	diffuse.rgb *= tint;
 
-	float contrast = 2.0;
-	diffuse.rgb = pow(diffuse.rgb, vec3(contrast));
+	diffuse.rgb *= TONE_MAPPING_TINT * TONE_MAPPING_BRIGHTNESS;
+
+	diffuse.rgb = pow(diffuse.rgb, vec3(TONE_MAPPING_CONTRAST));
 
 	// Tone compensation
-	diffuse.rgb = diffuse.rgb/(diffuse.rgb + vec3(1.0));
+	diffuse.rgb = diffuse.rgb / (diffuse.rgb + vec3(1.0));
 	
 	// Gamma correction
-	float gamma = 1.0;
-	diffuse.rgb = pow(diffuse.rgb, vec3(1.0 / gamma));
+	diffuse.rgb = pow(diffuse.rgb, vec3(1.0 / TONEMAPPING_GAMMA));
 
-	float saturation = 1.0;
 	float grayScale = (diffuse.r + diffuse.g + diffuse.b) / 3.0;
-	return mix(vec3(grayScale), diffuse.rgb, saturation);
+	return mix(vec3(grayScale), diffuse.rgb, TONE_MAPPING_SATURATION);
 }
