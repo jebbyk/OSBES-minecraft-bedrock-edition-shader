9c001d7c1052a190f723f49f88edf91475be31c2
Daniel Bradbury <danieljamesbradbury@hotmail.com>
2021-07-07 11:22:47 +0100

    TONEMAPPING_GAMA is now consistent with other tonemap variable names

--- shaders/glsl/includes/color_correction.glsl
+++ shaders/glsl/includes/color_correction.glsl
@@ -8,7 +8,7 @@ vec3 colorCorrection(vec3 diffuse){
 	diffuse.rgb = diffuse.rgb / (diffuse.rgb + vec3(1.0));
 	
 	// Gamma correction
-	diffuse.rgb = pow(diffuse.rgb, vec3(1.0 / TONEMAPPING_GAMMA));
+	diffuse.rgb = pow(diffuse.rgb, vec3(1.0 / TONE_MAPPING_GAMMA));
 
 	float grayScale = (diffuse.r + diffuse.g + diffuse.b) / 3.0;
 	return mix(vec3(grayScale), diffuse.rgb, TONE_MAPPING_SATURATION);
